19bf0e25599b0c0ca0f39a38c96bfbec6cd01b17
Gabriel Manussakis <Manussakis@users.noreply.github.com>
2021-04-20 10:30:28 -0300

    Add custom.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "normalize.css";
 import "./css/main.ltr.css";
 import "./css/themes.css";
 import "./css/purple.css";
+import "./css/custom.css";
 
 import SiteState from './js/SiteState';
 import Site from './js/Site';
